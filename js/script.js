c03af26ea2b143d416ae5925426770dbf3099305
Диана <dianka.kiselyova@mail.ru>
2020-12-03 00:07:19 +0300

    Добавлены js-файлы

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let btnDefault = document.querySelector(".btn-default");
+let intro = document.querySelector(".intro")
+
+btnDefault.addEventListener("clik", () => {alert("Щелчок по кнопке!"); });
+
+intro.style.border = "5px dashed gold";
+document.bode.style.backgroundColor = "lightgray";
